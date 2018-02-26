cf7bf40cbde8970da1e776246fa3df97a8c4f506
Issa Ennab <issaennab@Ashleys-MacBook-Air.local>
2018-02-25 20:37:33 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  title = 'Come2OurWedding';
+  weddingDay = new Date('2018-09-02');
+  test = "ISSA";
+
 }
